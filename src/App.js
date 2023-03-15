b4c02eb5e4ab635c49c23934ad0a0b6494b06d11
exoticdx <96467259+exoticdx@users.noreply.github.com>
2023-03-15 20:45:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import Navbar from './componants/Navbar';
+import Home from './componants/Home';
+import Dennis from './componants/dennis';
 import './App.css';
+import Contact from './componants/Contact';
+// import Ruff from './componants/ruff';
+// import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Aboutus from './componants/Aboutus';
+import ServicesArea from './componants/ServicesArea';
+// import ServicesArea from './componants/ServicesArea';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='mainapp'>
+        <div className="RouterDiv">
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<Aboutus/>}/>
+              <Route path='/services' element={<ServicesArea/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/dennis' element={<Dennis/>}/>
+            </Routes>
+          </Router>
+        </div>
+      </div >
+    </>
   );
 }
-
 export default App;
